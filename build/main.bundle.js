7223ab4868639c00ee74a94f5e1a9ca8531204d1
Alexander Adrian Guerra <alex@paper-rad.io>
2018-05-10 06:02:24 -0500

    Initial commit

---
+++ build/main.bundle.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var _lodash = require("lodash");
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var undefFromAfunction = _lodash2.default.max();
+
+var notUndifFromAfunction = _lodash2.default.max([1, 3]);
+
+console.log("hello");
+
+var myName = "alex";
+
+var greeting = "Hello";
+
+console.log(myName, " ", greeting);
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import _ from "lodash";
+
+let undefFromAfunction = _.max();
+
+let notUndifFromAfunction = _.max([1, 3]);
+
+console.log("hello");
+
+let myName = "alex";
+
+const greeting = "Hello";
+
+console.log(myName, " ", greeting);
