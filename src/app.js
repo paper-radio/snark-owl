22d86fdd0e7e569c1086320826497e93b80cb5b2
Alexander Adrian Guerra <alex@paper-rad.io>
2018-05-10 14:18:11 -0500

    Blank the src file

--- src/app.js
+++ src/app.js
@@ -1,19 +1,1 @@
 import _ from "lodash";
-
-let undefFromAfunction = _.max();
-
-let notUndifFromAfunction = _.max([1, 3]);
-
-console.log("The un should be ", undefFromAfunction);
-
-console.log("hello");
-
-let myName = "alex";
-
-const greeting = "Hello";
-
-console.log(myName, " ", greeting);
-
-const simpleFxn = () => 1;
-
-console.log((() => 2)());
